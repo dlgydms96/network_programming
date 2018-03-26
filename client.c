e465630366e08b97c37aa3d2945986ee6d6bb451
Unknown <dlgydms30@naver.com>
2018-03-26 13:12:22 +0900

    basic_client&server

---
+++ client.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main()
+{
+	int sockfd;
+	struct sockaddr_in addr;
+
+	sockfd = socket(AF_INET, SOCK_STREAM,0);
+	
+addr.sin_family= AF_INET;
+addr.sin_port = htons(13579);
+inet_aton("127.0.0.1",&addr.sin_addr);
+memset(&(addr.sin_zero),0,8);
+
+connect(sockfd, (struct sockaddr*)&addr,sizeof(addr));
+
+#define BUF_SIZE 10
+int str_len;
+int newfd;
+char message[BUF_SIZE];
+struct sockaddr_in client_addr;
+int addr_len;
+
+while(1){
+	printf("Input message(Q to quit): ");
+	fgets(message,BUF_SIZE,stdin);
+	if(strcmp(message,"q\n") == 0 || strcmp(message,"Q\n") == 0){
+	break;
+	}
+
+	write(sockfd,message,strlen(message));
+	str_len = read(sockfd,message,BUF_SIZE -1);
+	message[str_len]=0;
+	printf("Message from server : %s", message);
+}
+close(sockfd);
+}

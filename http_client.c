7a66d1bf7b2b10fb75313b3436fd7545d64419e7
Unknown <dlgydms30@naver.com>
2018-04-02 21:09:32 +0900

    에러 잡음

--- http_client.c
+++ http_client.c
@@ -2,40 +2,48 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 1000
 
 int main(int argc, char* argv[])
 {
+	int str_len;
+	char message[BUF_SIZE]={"GET /index.html HTTP/1.1\r\n\r\n"};
+
 	int sockfd;
 	struct sockaddr_in addr;
-
-	sockfd = socket(AF_INET, SOCK_STREAM,0);
-	
-addr.sin_family= AF_INET;
-addr.sin_port = htons(80);
-inet_aton(argv[1],&addr.sin_addr);
-memset(&(addr.sin_zero),0,8);
-
-printf("%s",argv[1]);
-connect(sockfd, (struct sockaddr*)&addr,sizeof(addr));
-
-#define BUF_SIZE 1000
-int str_len;
-int newfd;
-char message[BUF_SIZE]={"GET /index.html HTTP/1.1\r\n\r\n"};
-struct sockaddr_in client_addr;
-int addr_len;
-
-//while(1){
-//	printf("Input message(Q to quit): ");
-	//fgets(message,BUF_SIZE,stdin);
-/*	if(strcmp(message,"q\n") == 0 || strcmp(message,"Q\n") == 0){
-	break;
+	if(argc !=2){
+		printf("Usage : %s <IP> \n", argv[0]);
+		exit(1);
 	}
-*/
+	sockfd = socket(AF_INET, SOCK_STREAM,0);
+	if(sockfd ==-1)
+		error_handling("socket() error");
+	
+	addr.sin_family= AF_INET;
+	addr.sin_port = htons(80);
+	inet_aton(argv[1],&addr.sin_addr);
+	memset(&(addr.sin_zero),0,8);
+
+	if(connect(sockfd, (struct sockaddr*)&addr,sizeof(addr))==-1)
+		error_handling("connect() error!");
+
 	write(sockfd,message,strlen(message));
 	str_len = read(sockfd,message,BUF_SIZE -1);
-	message[str_len]=0;
+	if(str_len == -1) error_handling("read() error!");
+	
+//	message[str_len]=0;
 	printf("Message from server : %s", message);
-//}
-close(sockfd);
+
+	close(sockfd);
+	return 0;
+}
+
+void error_handling(char* msg)
+{
+	fputs(msg, stderr);
+	fputc('\n',stderr);
+	exit(1);
 }
